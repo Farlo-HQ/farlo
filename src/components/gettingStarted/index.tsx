145c536b8460d72fac9d2749567ffe5d92731503
The-Anomaly <favouradekogbe@gmail.com>
2025-05-11 23:30:01 +0100

    feat: hide getting started section

--- src/components/gettingStarted/index.tsx
+++ src/components/gettingStarted/index.tsx
@@ -4,16 +4,17 @@ import { Button } from "../button";
 
 const GettingStarted = ({ greyBg }: { greyBg?: boolean }) => {
   return (
-    <Section
-      bgClassName={greyBg ? styles.greyBg : ""}
-      sectionClassName={styles.section}
-    >
-      <div className={styles.header}>
-        <p>What are you waiting for?</p>
-        <h3 className="">Get started in three easy steps.</h3>
-      </div>
-      <div style={{ background: "grey", width: "100%", height: "500px" }}></div>
-    </Section>
+    <></>
+    // <Section
+    //   bgClassName={greyBg ? styles.greyBg : ""}
+    //   sectionClassName={styles.section}
+    // >
+    //   <div className={styles.header}>
+    //     <p>What are you waiting for?</p>
+    //     <h3 className="">Get started in three easy steps.</h3>
+    //   </div>
+    //   <div style={{ background: "grey", width: "100%", height: "500px" }}></div>
+    // </Section>
   );
 };
 export { GettingStarted };
