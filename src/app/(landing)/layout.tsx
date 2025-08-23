b09e9a7dfc88f8ab24b9c842e57f81e1b8058d8e
The-Anomaly <favouradekogbe@gmail.com>
2025-08-23 23:10:54 +0100

    feat: update project structure to add folder for pages with landing/website layout

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout";
+
+export default function LandingLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <Layout>{children}</Layout>;
+}
