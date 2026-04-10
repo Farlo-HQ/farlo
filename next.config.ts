a8f79701ba2749c498c8ad38ae8353fa14387664
adedolapoakanle <dolapoakanle@gmail.com>
2026-04-10 11:26:23 +0100

    fix: downgrade Next.js and fix turbopack config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // reactStrictMode: true,
+  turbopack: {},  
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule: any) =>
